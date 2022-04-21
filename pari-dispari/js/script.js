0cad191ee960ac5f96580ff96d7d76f25d39b800
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 15:07:48 +0200

    aggiunti prompt per input utente (senza controllo input per il momento)

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,11 +1,16 @@
 // l'utente deve scegliere pari o dispari
+const userEvenOdd = prompt("Ciao! Scegli pari o dispari..");
+console.log("userEvenOdd", userEvenOdd);
 
 // l'utente deve scegliere un numero tra 1 e 5
+const userNumber = parseInt(prompt("adesso scegli un numero tra 1 e 5.."));
+console.log("userNumber", userNumber);
 
 // generiamo un numero random tra 1 e 5 per il pc (con funzione)
+function randomNumber(min, max) 
 
 // sommiamo i due numeri
 
 // stabiliamo se la somma dei due numeri è pari o dispari
 
-// stabiliamo chi ha vinto in base a cosa ha scelto lìutente tra pari e dispari
+// stabiliamo chi ha vinto in base a cosa ha scelto l'utente tra pari e dispari
