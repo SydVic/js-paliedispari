709481749b94789a35c2831a281d07770e7de101
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 14:51:33 +0200

    aggiunto pseudocodice

---
+++ pari-dispari/js/script.js
@@ -0,0 +1,11 @@
+// l'utente deve scegliere pari o dispari
+
+// l'utente deve scegliere un numero tra 1 e 5
+
+// generiamo un numero random tra 1 e 5 per il pc (con funzione)
+
+// sommiamo i due numeri
+
+// stabiliamo se la somma dei due numeri è pari o dispari
+
+// stabiliamo chi ha vinto in base a cosa ha scelto lìutente tra pari e dispari
