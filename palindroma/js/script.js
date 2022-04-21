1c198bbb0e4a192e347ca1790a8a7fbe10e81faf
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 14:26:50 +0200

    trovata possibile soluzione

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -5,7 +5,7 @@ console.log("userWord",userWord);
 //veridfico se la parola è palindroma tramite la funzione
 palindromeWord(userWord);
 
-console.log("userWord 0",userWord[0]);
+//console.log("userWord 0",userWord[0]);
 
 // creo una funzione per capire se la parola è palindroma
 //      -trovare un modo per confrontare se gli elementi della stringa letti dall'inizio alla fine sono uguali agli elementi letti dalla fine all'inizio
@@ -22,4 +22,17 @@ function palindromeWord(wordToCheck) {
     // e cosi via -> wordToCheck[1] sia === a wordToCheck[2]
 
     // oppure scompongo dall'inizio alla fine e pusho in un array e poi faccio lo stesso dalla fine all'inizio e confronto i due risultati?
+    // ok non mi serve l'array!
+    let wordSplitted = "";
+    for (let i=0; i < wordToCheck.length; i++) {
+        wordSplitted += wordToCheck[i];
+        console.log("wordSplitted", wordSplitted, typeof(wordSplitted));
+    }
+
+    // reversed word
+    let wordReversed ="";
+    for (let i = (wordToCheck.length - 1); i >= 0; i--) {
+        wordReversed += wordToCheck[i];
+        console.log("wordReversed", wordReversed, typeof(wordReversed));
+    }
 }
