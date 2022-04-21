3fd5ceeebc8f09c79f781fd963a40b72317de172
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 13:46:40 +0200

    abbozzo pseudocodice

---
+++ palindroma/js/script.js
@@ -0,0 +1,6 @@
+// chiedo all' utente di inserire una parola
+const userWord = prompt("Dimmi una parola");
+console.log("userWord",userWord);
+
+// creo una funzione per capire se la parola è palindroma
+//      -trovare un modo per confrontare se gli elementi della stringa letti dall'inizio alla fine sono uguali agli elementi letti dalla fine all'inizio
