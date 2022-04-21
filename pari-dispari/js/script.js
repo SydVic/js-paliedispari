807a7bb5844b5fe2b4b0cd63da68a6e162b35685
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 15:53:50 +0200

    aggiunta funzione per pari o dispari

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -11,12 +11,16 @@ const randomPcNumber = randomNumber(1, 5);
 console.log("randomPcNumber", randomPcNumber);
 
 // sommiamo i due numeri
+const summedNumber = userNumber + randomPcNumber;
+console.log("summedNumber", summedNumber);
 
 // stabiliamo se la somma dei due numeri è pari o dispari (con funzione)
+evenOrOdd = evenOddNumber(summedNumber);
+console.log("evenOrOdd", evenOrOdd);
 
 // stabiliamo chi ha vinto in base a cosa ha scelto l'utente tra pari e dispari
 
-
+// FUNCTIONS
 /**
  * Description generate a random number betwenn the min e max parameter
  * @param {Number} min -> the minimum number we want to generate
@@ -26,4 +30,19 @@ console.log("randomPcNumber", randomPcNumber);
  function randomNumber(min, max) {
     const generatedRandomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
     return generatedRandomNumber;
+}
+
+/**
+ * Description the function check if the number is even or odd
+ * @param {any} numberToCheck -> the number you want to know if is even or odd
+ * @returns {String} -> return "pari" if the number is even and "dispari" if the number is odd
+ */
+function evenOddNumber(numberToCheck) {
+    let result = "";
+    if ( numberToCheck % 2 === 0) {
+        result = "pari";
+    } else {
+        result = "dispari";
+    }
+    return result;
 }
