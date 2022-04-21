2b679bdb09cb7bc15c2c41adc32de9ffcf74dc61
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 14:43:07 +0200

    aggiunta stampa del risultato in console log

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,13 +3,11 @@ const userWord = prompt("Dimmi una parola");
 console.log("userWord",userWord);
 
 //veridfico se la parola è palindroma tramite la funzione
-palindromeWord(userWord);
-
-//console.log("userWord 0",userWord[0]);
-
-// creo una funzione per capire se la parola è palindroma
-//      -trovare un modo per confrontare se gli elementi della stringa letti dall'inizio alla fine sono uguali agli elementi letti dalla fine all'inizio
-
+if (palindromeWord(userWord)) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
 
 /**
  * Description the function check if the word is a palindrome word or not
@@ -18,21 +16,17 @@ palindromeWord(userWord);
  */
 function palindromeWord(wordToCheck) {
     console.log("wordToCheck", wordToCheck);
-    // mettiamo caso che parola sia di 4 lettere, come posso fare per confrontare che wordToCheck[0] sia === a wordToCheck[3]
-    // e cosi via -> wordToCheck[1] sia === a wordToCheck[2]
-
-    // oppure scompongo dall'inizio alla fine e pusho in un array e poi faccio lo stesso dalla fine all'inizio e confronto i due risultati?
-    // ok non mi serve l'array!
-    let wordSplitted = "";
-    for (let i=0; i < wordToCheck.length; i++) {
-        wordSplitted += wordToCheck[i];
-        console.log("wordSplitted", wordSplitted, typeof(wordSplitted));
-    }
-
-    // reversed word
-    let wordReversed ="";
+    // inverto la parola con un ciclo for leggendola dalla fine all'inizio
+    let reversedWord ="";
     for (let i = (wordToCheck.length - 1); i >= 0; i--) {
-        wordReversed += wordToCheck[i];
-        console.log("wordReversed", wordReversed, typeof(wordReversed));
+        reversedWord += wordToCheck[i];
     }
+    console.log("reversedWord", reversedWord, typeof(reversedWord));
+
+    // controllo se la parola invertita è === a quella inserita dall'utente
+    let isPalindromeWord = false;
+    if (reversedWord === wordToCheck) {
+        isPalindromeWord = true;
+    }
+    return isPalindromeWord;
 }
