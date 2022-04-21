6e1854fe9284f4d66a8c2a913c499d9a61cc39f4
SydVic <fabrizio.bocca27@gmail.com>
2022-04-21 14:01:45 +0200

    check per impostazione funzione

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,5 +2,18 @@
 const userWord = prompt("Dimmi una parola");
 console.log("userWord",userWord);
 
+//veridfico se la parola è palindroma tramite la funzione
+palindromeWord(userWord);
+
 // creo una funzione per capire se la parola è palindroma
 //      -trovare un modo per confrontare se gli elementi della stringa letti dall'inizio alla fine sono uguali agli elementi letti dalla fine all'inizio
+
+
+/**
+ * Description the function check if the word is a palindrome word or not
+ * @param {String} wordToCheck
+ * @returns {Boolean}
+ */
+function palindromeWord(wordToCheck) {
+    console.log("wordToCheck", wordToCheck);
+}
